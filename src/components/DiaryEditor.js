df84e53b25c18cd9175f452463f096da8a4d15ad
se0983 <se098300@gmail.com>
2024-01-27 18:15:10 +0900

    feat: 일기 저장 버튼 클릭 시 정상 입력 아니라면 focus 주기(useRef)

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,6 +1,10 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const DiaryEditor = () => {
+
+    const authorInput = useRef();
+    const contentInput = useRef();
+
     const [state, setState] = useState({
         author: "",
         content: "",
@@ -15,7 +19,16 @@ const DiaryEditor = () => {
     };
 
     const handleSubmit = () => {
-        console.log(state);
+        if(state.author.length < 1){
+            // focus
+            authorInput.current.focus();
+            return;
+        }
+        if(state.content.length < 5){
+            // focus
+            contentInput.current.focus();
+            return;
+        }
         alert("저장 성공!");
     };
 
@@ -24,6 +37,7 @@ const DiaryEditor = () => {
             <h2>오늘의 일기</h2>
             <div>
                 <input 
+                    ref={authorInput}
                     name="author" 
                     value={state.author} 
                     onChange={handleChangeState}
@@ -33,6 +47,7 @@ const DiaryEditor = () => {
             </div>
             <div>
                 <textarea
+                    ref={contentInput}
                     name="content"
                     value={state.content} 
                     onChange={handleChangeState}
